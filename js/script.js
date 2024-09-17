80f668dc2668cb0631501f98a76b76ac0de9f541
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-09-17 09:03:21 -0700

    logic to show different tabs

--- js/script.js
+++ js/script.js
@@ -6,3 +6,31 @@ const dataResponse = await fetch("data.json");
 const data = await dataResponse.json();
 
 console.log(data);
+
+const inputs = document.querySelectorAll("input[type='radio']");
+const movieTemplate = document.getElementById("movie-template");
+const tvTemplate = document.getElementById("tv-template");
+const bookmarksTemplate = document.getElementById("bookmarks-template");
+const home = document.getElementById("home");
+const searchInput = document.getElementById("search");
+const mainEle = document.querySelector("main");
+
+const templates = {
+  home: home,
+  tv: tvTemplate,
+  movie: movieTemplate,
+  bookmark: bookmarksTemplate,
+};
+// const clone = movieTemplate.content.cloneNode(true);
+// console.log(clone);
+function changePage(e) {
+  searchInput.setAttribute("data-search", e.currentTarget.id);
+  mainEle.replaceChild(
+    templates[e.currentTarget.id].content.cloneNode(true),
+    mainEle.children[1]
+  );
+}
+
+for (const input of inputs) {
+  input.addEventListener("click", changePage);
+}
