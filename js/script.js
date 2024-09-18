81719f4c39a69958d3ef404d37dcf3f074d12c59
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-09-17 20:09:19 -0700

    WIP bookmark logic

--- js/script.js
+++ js/script.js
@@ -264,15 +264,12 @@ function updateCards(id, clone) {
             mediumObj.title
           }" data-category="${mediumObj.category}">
               <div class="bookmark-con">
-                <img
-                  src=${
-                    mediumObj.isBookmarked
-                      ? "./assets/icon-bookmark-full.svg"
-                      : "./assets/icon-bookmark-empty.svg"
-                  }
-                  alt='${
-                    mediumObj.isBookmarked ? "bookmarked" : "not bookmarked"
-                  }'/>
+                 ${
+                   mediumObj.isBookmarked
+                     ? `<svg  alt="bookmarked" width="12" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M10.61 0c.14 0 .273.028.4.083a1.03 1.03 0 0 1 .657.953v11.928a1.03 1.03 0 0 1-.656.953c-.116.05-.25.074-.402.074-.291 0-.543-.099-.756-.296L5.833 9.77l-4.02 3.924c-.218.203-.47.305-.756.305a.995.995 0 0 1-.4-.083A1.03 1.03 0 0 1 0 12.964V1.036A1.03 1.03 0 0 1 .656.083.995.995 0 0 1 1.057 0h9.552Z" stroke='currentColor' stroke-width='1.5' fill="currentColor"/></svg>`
+                     : `<svg alt="not bookmarked" width='12' height='14' xmlns='http://www.w3.org/2000/svg'><path d='m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z' stroke='currentColor' stroke-width='1.5' fill='currentColor'/></svg>`
+                 }
+               
               </div>
               <picture>
                 <source
@@ -424,15 +421,12 @@ function updateCards(id, clone) {
               mediumObj.title
             }" data-category="${mediumObj.category}">
               <div class="bookmark-con">
-                <img
-                  src=${
-                    mediumObj.isBookmarked
-                      ? "./assets/icon-bookmark-full.svg"
-                      : "./assets/icon-bookmark-empty.svg"
-                  }
-                  alt='${
-                    mediumObj.isBookmarked ? "bookmarked" : "not bookmarked"
-                  }'   />
+              ${
+                mediumObj.isBookmarked
+                  ? `<svg  alt="bookmarked" width="12" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M10.61 0c.14 0 .273.028.4.083a1.03 1.03 0 0 1 .657.953v11.928a1.03 1.03 0 0 1-.656.953c-.116.05-.25.074-.402.074-.291 0-.543-.099-.756-.296L5.833 9.77l-4.02 3.924c-.218.203-.47.305-.756.305a.995.995 0 0 1-.4-.083A1.03 1.03 0 0 1 0 12.964V1.036A1.03 1.03 0 0 1 .656.083.995.995 0 0 1 1.057 0h9.552Z" fill="currentColor"/></svg>`
+                  : `<svg alt="not bookmarked" width='12' height='14' xmlns='http://www.w3.org/2000/svg'><path d='m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z' stroke='currentColor' stroke-width='1.5' fill='currentColor'/></svg>`
+              }
+               
               </div>
               <picture>
                 <source
@@ -482,7 +476,8 @@ function changePage(e) {
   updateCards(e.currentTarget.id, clone);
 
   clone.children[0].addEventListener("click", (e) => {
-    if (e.target.tagName === "IMG") {
+    console.log(e.target.tagName);
+    if (e.target.tagName === "SVG") {
       const altText = e.target.getAttribute("alt");
       if (altText.startsWith("bookmarked")) {
         e.target.setAttribute("src", "./assets/icon-bookmark-empty.svg");
